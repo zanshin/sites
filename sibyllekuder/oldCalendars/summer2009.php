c0fde5fb23c61f87521f02d751a00baf6aff12ad
Mark Nichols <mhn@ksu.edu>
2009-11-08 19:57:44 -0600

    archiving old calendar pages removing links to old calendar pages only the password protected calendar.php remains

--- sibyllekuder/oldCalendars/summer2009.php
+++ sibyllekuder/oldCalendars/summer2009.php
@@ -1,11 +1,10 @@
-<?php include("/home/markhnic/public_html/sibyllekuder.com/password_protect.php"); ?>
 <!DOCTYPE html PUBLIC 
 	"-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Elfenbein Klaviermusik - Piano Instruction for the Dedicated Student</title>
+	<title>Elfenbein Klaviermusik - Piano Instruction for the Dedicated Studentfli</title>
 	<meta name="keywords" content="piano, piano teaching, piano instruction, private piano instruction, piano performance, Manhattan, Junction City, Wamego, Riley, Ogden" />
 	<meta name="description" content="Private piano studio in Manhattan Kansas for dedicated students of all ages." />
 	<meta name="author" content="sibylle kuder" />
@@ -15,7 +14,6 @@
 	<meta name="robots" content="all" />
 	<link rel="shortcut icon" href="img/favicon.ico" type="image/ico" />
 	<link href="style.css" rel="stylesheet" type="text/css" />
-	<script src="/js/protect.js" type="text/javascript"></script>
 	<script src="/mint/?js" type="text/javascript"></script>
 
 </head>
@@ -31,27 +29,26 @@
 	</div>
 	<div id="content">
 		
-		<h1>Fall 2009 Calendar</h1>
+		<h1>Summer 2009 Calendar</h1>
 		<p>
-		August 18 - December 19
+		June 1 - July 31
+		</p>
+		<p align="center">
+		private piano lessons . group lessons . performance classes . special events
 		</p>
 		<p>
-		Enrollment packages for the Fall 2009 semester are now available, please fill out your 
-		<a href="http://sibyllekuder.com/pdfs/Fall2009SchedulingWorksheet.pdf" target="_blank" 
-		title="Fall 2009 Scheduling Worksheet PDF">schedule preferences worksheet</a> and return 
-		it as soon as possible. Fall lessons will start August 18th, the tuition is due August 1st.
+		All students are required to schedule a minimum of eight 30-minute lessons or six 45-minute 
+		lessons or four hour-long lessons during the summer session in order to retain their spot in 
+		the studio for the fall semester. Special arrangements will be made for students who are out of 
+		the country for more than four weeks during the summer.
 		</p>
 		<p>
-		<a href="http://www.google.com/calendar/embed?src=4ijrva5ro332q65c414bhttbfk%40group.calendar.google.com&ctz=America/Chicago" title="Fall 2009 lesson and event calendar." target="_blank">Please click here to view the Fall 2009 Calendar</a>. (This link will open in a new window.) This calendar will be updated with any schedule changes or
-		added events, please check back frequently. Dates for recitals and performance classes to be 
-		announced.
+		Students are guaranteed the equivalent of eight 30-minute private piano lessons but are 
+		encouraged to attend as many lessons and group events as they wish and scheduling permits.
 		</p>
-		<!--
 		<p>
-		<a href="http://www.google.com/calendar/embed?src=4ijrva5ro332q65c414bhttbfk%40group.calendar.google.com&ctz=America/Chicago" title="Summer 2009 lesson and event calendar." target="_blank">Please click here to view the Summer 2009 Calendar</a>. (This link will open in a new window.) This calendar will be updated with any schedule changes or
-		added events, please check back frequently.
+		Summer tuition (all-inclusive): $130
 		</p>
-		-->
 		<br />
 		<!--
 		<div class="callout right">
@@ -61,18 +58,29 @@
 		-->
 		<br />
 		<h1>Lesson Details</h1>
-		<!--
 		<p>
 		Summer hours are 10:30 a.m. to 7:00 p.m.  To aid you in selecting the lesson schedule you 
 		prefer I have created a <a href="http://sibyllekuder.com/pdfs/SummerSchedule2009.pdf" target="_blank" 
 		title="Summer Schedule 2009 PDF">schedule worksheet</a> that you can 
 		download and fill out. 
 		</p>
-		-->
 		<p>
 			Festivals, auditions, and competitions are routinely held on weekends.
 		</p>
 		<br /><br />
+		<h1>Previous Calendars</h1>
+		<ul>
+			<!--
+			<li><a href="fall2007.php" title="Fall 2007 Calendar" target="_blank">Fall 2007 Calendar</a></li>
+			-->
+			<li><a href="spring2008.php" title="Spring 2008 Calendar" target="_blank">Spring 2008 Calendar</a></li>
+			<!--
+			<li><a href="summer2008.php" title="Summer 2008 Calendar" target="_blank">Summer 2008 Calendar</a></li>
+			-->
+			<li><a href="fall2008.php" title="Fall 2008 Calendar" target="_blank">Fall 2008 Calendar</a></li>
+			<li><a href="spring2009.php" title="Spring 2009 Calendar" target="_blank">Spring 2009 Calendar</a></li>
+		</ul>
+
 		<br />
 		<br />
 		<div id="navcontainer">
---
+++ sibyllekuder/oldCalendars/summerprogram2008.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC
+	"-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Elfenbein Klaviermusik - Piano Instruction for the Dedicated Student</title>
+	<meta name="keywords" content="piano, piano teaching, piano instruction, private piano instruction, piano performance, Manhattan, Junction City, Wamego, Riley, Ogden" />
+	<meta name="description" content="Private piano studio in Manhattan Kansas for dedicated students of all ages." />
+	<meta name="author" content="sibylle kuder" />
+	<meta name="copyright" content="Copyright 2007-2009 Sibylle Kuder" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta name="revisit-after" content="3 days" />
+	<meta name="robots" content="all" />
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/ico" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<script src="/mint/?js" type="text/javascript"></script>
+
+</head>
+
+<body>
+	<div id="container">
+		<div id="header">
+			<?php include("header.php")?>
+		</div>
+
+	<div id="navcontainer">
+		<?php include("navigation.php")?>
+	</div>
+	<div id="content">
+		<h1>Piano Summer 2008</h1>
+		<div class="callout right">
+			<img src="img/grandpianopainting.jpg" height="236" width="333" alt="grand piano montage painting"/>
+		</div>
+		<p>
+			June 2 - July 31, 2008
+		</p>
+		<p>
+			Summer tuition is all-inclusive; students are guaranteed eight lessons but are encouraged 
+			to attend as many lessons and classes as they wish and scheduling permits. 
+			This year's program includes:
+		</p>
+		<p class="indent">
+			Eight 30-minute private<sup>*</sup> piano lessons in your home, to be scheduled at your and
+			my convenience - there is much flexibility in the summer: lesson days and times
+			can change if necessary (and possible), lessons don't always have to happen on the
+			same weekday or at the same time.  I look forward to working around your 
+			vacation schedule!  <br /><sup>*</sup> Private means one-on-one. If appropriate, six 45-minute lessons 
+			may be substituted for eight 30-minute lessons.
+		</p>
+		<p class="indent">
+			Additional lessons are offered in my home at no additional charge, to be scheduled
+			at my convenience.
+		</p>
+		<p class="indent">
+			Group events!  This year's focus is on the Russian composer Prokofiev. Some of the
+			planned events are: <i>Peter and the Wolf</i> (introduction to the instruments of
+			the orchestra, learning to play some of the tunes on the pinao, both by ear and
+			from the score, etc.)  <i>Romeo and Juliet</i> and/or <i>Cinderella</i> (
+			introduction to ballet, listening to Prokofiev's arrangements of some of the
+			ballet).  Prokofiev's life, his piano music, and, for advanced students, his 
+			orchestral music (Classical Symphony, Piano Concertos, Violin Concertos).
+		</p>
+		<p class="indent">
+			Performance classes.
+		</p>
+		<p>
+			Summer tuition: $210 - covers all of the above.  Tuition is due June 1st.
+		</p>
+		<br />
+		<div id="navcontainer">
+			<?php include("navigation.php")?>
+		</div>
+	</div>
+	<div id="footer">
+		<?php include("footer.php")?>
+	</div>
+	</div>
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+		_uacct = "UA-373283-1";
+
+
+		urchinTracker();
+	</script>
+</body>
+</html>
