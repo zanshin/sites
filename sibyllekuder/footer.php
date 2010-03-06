1ab4df89449556cab571a1d017fce293fe7de2bb
Mark Nichols <mhn@ksu.edu>
2010-03-06 11:13:34 -0600

    updated footed email address to new piano account on domain

--- sibyllekuder/footer.php
+++ sibyllekuder/footer.php
@@ -32,23 +32,31 @@
 <h1>
 	<div class="left">Copyright &copy; 2007-2009 
 		<script type="text/javascript">
-		/* <![CDATA[ */
-		function hivelogic_enkoder(){var kode=
-		"kode=\")''(nioj.)(esrever.)''(tilps.edok=edok;\\\"kode=\\\"\\\\nrgh@%rnhg_"+
-		"%@nrgh_%__@@g{nh,r+\\\\\\\\000gfFhdrFurkipjul1wq@u{V;.4>.5,@?f+3lf6i,>+0Dl"+
-		"gwFhdrfuhkr1@g~n.fl,k.j>hw1qgonhlr3?l>u@i+*r@>>*__{___%/__DA5(xgjBQkk{r&hr"+
-		"Y\\\\\\\\177(oxDjbQkk{r&hrY\\\\\\\\177rog&Kobsk(zCzr(oq&ybsok{|xro&goQhtkk"+
-		"rt&ltKjmgokxCmiXpz{kEhuy4soisrFgkm{xkjrqhry\\\\\\\\177uor@gz(oCskbnlgx(&kB"+
-		"o.}zzxk4{tus__ij____@g_%nh>r_%__nrgh@nrgh1vsolw+**,1uhyhuvh+,1mrlq+**,>_%@"+
-		"{**i>url+3@l>+?rnhgo1qhwj0k,4l>@.,5{~@.rnhgf1dkDu+w.l,4n.gr1hkfudwDl+\\\\"+
-		"\\\\000,rnhg{@+.?lrnhgo1qhwjBkrnhgf1dkDu+wrnhgo1qhwj0k,4*=,*>%>{@**>iru+l@"+
-		"3>l?+nrgh1ohqjwk04,>l.@5,~{.@nrgh1fkduDw+l.4,.nrgh1fkduDw+l,\\\\\\\\000nrg"+
-		"h@{.+l?nrgh1ohqjwkBnrgh1fkduDw+nrgh1ohqjwk04,=**,>\\\"\\\\;x='';for(i=0;i<"+
-		"kode.length;i++){c=kode.charCodeAt(i)-3;if(c<0)c+=128;x+=String.fromCharCo"+
-		"de(c)}kode=x\\\"=edok\";kode=kode.split('').reverse().join('')"
-		;var i,c,x;while(eval(kode));}hivelogic_enkoder();
-		/* ]]> */
+		//<![CDATA[
+
+		function hiveware_enkoder(){var i,j,x,y,x=
+		"x=\"783d227d4227372535633a683b7d42683b61273b3a3b393b673c6b3b3a3b383c393b3a" +
+		"3b3a3c693b37373a3b6a3b6a3c383b39376b3b6a3c693a3c3c6837373b37383e3c6a3c393b" +
+		"353b3a373e3b3d37363b37386a3b683b6b393637353c353b383b3d3c3e3b373b373c3a3b3e" +
+		"3b3b38683b693a683b68373a3b373b673c693b3a3b363b393b3e3b3a3c683c3737393b6a3b" +
+		"6b38383b663c6b3b353b69383e3b6837363b6b3b6a3b36386b396a37353c3737383b3e383e" +
+		"3b6738373c35383e3b676127683b407e683b422c3a3b2c40673c6b743a3b772d393b6e423a" +
+		"3c354037376e416a3b7d33383b716a6b3b736c693a796d6837406e373c3042393b372e3e3c" +
+		"2532327e393b3042683b7a733a386a78693a68666837756a373c2d2c353b2a2c3e3b307d36" +
+		"3b33786a3b7a676b397879353c772d383b6e313e3b372e373c2e403e3b247e27406f426a7b" +
+		"66712d7d33686d667746792d352e2e407d427d33787a677879772d362e407e422c2c406b74" +
+		"772d6e4235406e417d33716a736c796d406e3042392e2532327e30427d33787a677879772d" +
+		"6e31372e40246b74772d6e4237406e417d33716a736c796d406e3042392e2532327e30427d" +
+		"33787a677879772d6e31372e40247e427e33787a677879772d6f2e40223b793d27273b783d" +
+		"756e6573636170652878293b666f7228693d303b693c782e6c656e6774683b692b2b297b6a" +
+		"3d782e63686172436f646541742869292d353b6966286a3c3332296a2b3d39343b792b3d53" +
+		"7472696e672e66726f6d43686172436f6465286a297d79\";y='';for(i=0;i<x.length;i" +
+		"+=2){y+=unescape('%'+x.substr(i,2));}y";
+		while(x=eval(x));}hiveware_enkoder();
+
+		//]]>
 		</script>
+		
 		. All rights reserved.</div>
 	<div class="right">Website designed and maintained by <a href="http://zanshin.net" title="Mark Nichols" target="_blank">Mark Nichols</a></div>
 </h1>
